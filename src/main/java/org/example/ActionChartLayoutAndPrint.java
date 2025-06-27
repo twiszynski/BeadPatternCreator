8e4b0d68b8493855670bc3630adc7a89a173d390
twiszynski <tomaszwiszynski@gmail.com>
2025-06-28 01:03:29 +0200

    added action for existin pattern file completion with chart, layout and PDF print

--- src/main/java/org/example/ActionChartLayoutAndPrint.java
+++ src/main/java/org/example/ActionChartLayoutAndPrint.java
@@ -1,13 +1,10 @@
 package org.example;
 
-public class ActionFullWithPrint {
+public class ActionChartLayoutAndPrint {
 
     public static void main(String[] args) {
-        //ImageColorReplacement.main(args);
-        ImageToPatternAndLegend.main(args);
         WordChartGenerator.main(args);
         PatternToPageMap.main(args);
         PdfPrinter.main(args);
-        BeadVisualizer.main(args);
     }
 }
